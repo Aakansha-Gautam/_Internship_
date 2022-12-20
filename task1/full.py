f47591c8ee97015f9928032ff3bb1695661c9d9c
Aakansha2663 <aakanshagautam12@gmail.com>
2022-12-20 12:01:35 +0545

    task1 added

--- task1/full.py
+++ task1/full.py
@@ -2,5 +2,6 @@
 def full_image(path,query,driver):
     height = driver.execute_script("return document.body.parentNode.scrollHeight")
     width = driver.execute_script("return document.body.parentNode.scrollWidth")
-    driver.set_window_size(width, height)
+    # driver.set_window_size(width, height)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     driver.save_screenshot(f'{path}\\{query}\\_full.png')
