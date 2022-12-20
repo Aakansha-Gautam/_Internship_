0aef7436b6dcc53396114d06be3cd723286764b6
Aakansha-Gautam <118713447+Aakansha-Gautam@users.noreply.github.com>
2022-12-20 14:46:02 +0530

    Delete task2 directory

--- task2/full.py
+++
@@ -1,6 +0,0 @@
-
-def full_image(path,query,driver):
-    height = driver.execute_script("return document.body.parentNode.scrollHeight")
-    width = driver.execute_script("return document.body.parentNode.scrollWidth")
-    driver.set_window_size(width, height)
-    driver.save_screenshot(f'{path}\\{query}\\_full.png')
